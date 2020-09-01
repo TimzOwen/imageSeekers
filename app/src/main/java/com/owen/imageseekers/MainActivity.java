21209ebbf543bda6e2619fdd7c6e5fbb55080538
TimzOwen <ktimothy@kabarak.ac.ke>
2020-09-01 11:04:16 +0300

    soln to onclick seekbar listener'

--- app/src/main/java/com/owen/imageseekers/MainActivity.java
+++ app/src/main/java/com/owen/imageseekers/MainActivity.java
@@ -2,7 +2,10 @@ package com.owen.imageseekers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Step 1 . find the IDs from XML
+        //Step 1 . find the IDs from XML and add image.
+        //Adding image from here because we want it only to appear at run time.
+        final ImageView img_large = findViewById(R.id.image_view_large);
+        img_large.setImageResource(R.drawable.drawing);
 
+        //Step 2. find the seeker and associate a onClick listener
+        SeekBar seekBar = findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //step 3. declare a variable to hold our last value
+            int lastprogress;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                lastprogress = progress;
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //set the color filter using argb.
+                img_large.setColorFilter(Color.argb(255,0,lastprogress,255-lastprogress));
+            }
+        });
     }
 }
